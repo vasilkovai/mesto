7c1b2f66add5f1449c058b142e1878b186504db7
Irina Vasilkova <i.vasilkova.v@ya.ru>
2021-03-23 15:12:09 +0300

    fixes

--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,19 @@ let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_text_name');
 let aboutInput = document.querySelector('.popup__input_text_about');
 
-
+//Открытие формы для редактирования профиля
 function openPopup() {
   nameInput.value = nameProfile.textContent;
   aboutInput.value = aboutProfile.textContent;
   popup.classList.add('popup_opened');
 }
 
+//Закрытие формы для редактирования профиля
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
+//Редактирование профиля
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
